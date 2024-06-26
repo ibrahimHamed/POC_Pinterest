795e0fdf1a0a124b4a550f25045160906c991ebd
Ibrahim Hamed <ihamed@ntgclarity.com>
2024-06-26 13:53:49 +0300

    Initial Commit

---
+++ POC-PinterestLayout/POC_PinterestLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_PinterestLayoutApp.swift
+//  POC-PinterestLayout
+//
+//  Created by Ibrahim Hamed on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct POC_PinterestLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
